76022b1d4cf7eb08a92afddbf9667d2b588e3e37
Artyom Udovichenko <APTEM-1986@yandex.ru>
2018-12-16 11:45:09 +0300

    Remove req files

---
+++ Python/module01/3.6/vars.py
@@ -0,0 +1,9 @@
+str_var='MyString'
+int_var = 1
+fl_var = 12.5
+
+print('Variable name: "{0}", value: "{1}", type:{2}'.format('str_var', str_var, type(str_var)))
+print('Variable name: "{0}", value: "{1}", type:{2}'.format('int_var', int_var, type(int_var)))
+print('Variable name: "{0}", value: "{1}", type:{2}'.format('fl_var', fl_var, type(fl_var)))
+
+print(f'Variable name: "str_var", value: "{str_var}", type: "{type(str_var)}"')
---
+++ Python/module01/3.6/zen.py
@@ -0,0 +1,5 @@
+print('''Hello world. This is `The zen of Python:`
+
+....
+      
+I love it''')
